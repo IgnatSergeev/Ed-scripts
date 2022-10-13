90c34b1c00cbe3cc8bd166cef37e90a3b17e5af6
IgnatSergeev <ignatii.f.sergeev@gmail.com>
2022-10-14 00:29:33 +0300

    Added file read and write as well as system request

---
+++ main.py
@@ -0,0 +1,26 @@
+import requests
+
+def numberOfStars(system):#TODO
+    return 7
+
+with open("systems.txt", 'r') as systemsFile:
+    for line in systemsFile:
+        pass
+    last_line = line
+    lastPage = str(int(line) + 1)
+    print(int(lastPage) - 1)
+
+response = requests.get("https://eddbapi.elitebgs.app/api/v4/systems?permit=false&page="+lastPage)
+numOfPages = response.json()["pages"]
+for page in range(int(lastPage), numOfPages):
+    print(page)
+    response = requests.get("https://eddbapi.elitebgs.app/api/v4/systems?permit=false&page=" + str(page))
+    for system in response.json()["docs"]:
+        numberOfStarsInCur = numberOfStars(system)
+        if (numberOfStarsInCur > 6):
+            with open("systems.txt", 'a') as systemsFile:
+                systemsFile.write(system["name_lower"] + '\n')
+    with open("systems.txt", 'a') as systemsFile:
+        systemsFile.write(str(page) + '\n')
+
+
