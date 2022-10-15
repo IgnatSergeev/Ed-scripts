cc37a83e1453542d424e05f3db0df34f5b6a4ad0
IgnatSergeev <ignatii.f.sergeev@gmail.com>
2022-10-15 16:09:32 +0300

    Finished gzip and added max bodies

--- systemsGzipReading.py
+++ systemsGzipReading.py
@@ -1,13 +1,16 @@
 import json
 import gzip
 
-with gzip.open("../../Downloads/ALL/galaxy_1month.json.gz", 'r') as allSystemsFile:
+with gzip.open("E:\Elite galaxy map\galaxy.json.gz", 'r') as allSystemsFile:
     numOfLine = 0
     prevNumOfLine = 1
     maxNumOfStars = 0
+    maxNumOfBodies = 0
     for byteLine in allSystemsFile:
         numOfLine += 1
         line = byteLine.decode("utf-8")
+        if numOfLine < 10:
+            print(line)
         if numOfLine > 1:
             try:
                 jsonObj = json.loads('{ "info":' + line + '"empty":0}')
@@ -18,16 +21,27 @@ with gzip.open("../../Downloads/ALL/galaxy_1month.json.gz", 'r') as allSystemsFi
             name = jsonObj["info"]["name"]
             bodies = jsonObj["info"]["bodies"]
             numOfStars = 0
+            numOfBodies = 0
             for body in bodies:
                 if body["type"] == 'Star':
                     numOfStars += 1
+                numOfBodies += 1
+
+            if numOfBodies == maxNumOfBodies:
+                with open("bodies.txt", 'a') as systemsFile:
+                    systemsFile.write(name + ' $' + str(numOfBodies) + '$\n')
+            if numOfBodies > maxNumOfBodies:
+                maxNumOfBodies = numOfBodies
+                print("Max num of bodies = ", maxNumOfBodies, numOfLine)
+                with open("bodies.txt", 'w') as systemsFile:
+                    systemsFile.write(name + ' $' + str(numOfBodies) + '$\n')
 
             if numOfStars == maxNumOfStars:
                 with open("systems3.txt", 'a') as systemsFile:
                     systemsFile.write(name + ' $' + str(numOfStars) + '$\n')
             if numOfStars > maxNumOfStars:
                 maxNumOfStars = numOfStars
-                print(maxNumOfStars, numOfLine)
+                print("Max num of stars = ",maxNumOfStars, numOfLine)
                 with open("systems3.txt", 'w') as systemsFile:
                     systemsFile.write(name + ' $' + str(numOfStars) + '$\n')
 
